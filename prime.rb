02fb4d55b60aa6ce835a1a45ee59d58fb1af0fed
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 20:55:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  
+ if num <= 1
+   return false
+ else
+   range = (2.. num - 1)
+   range.none? {|i| num % i == 0}
+  end
+ end
+  
+#   i = 2 
+#   if i > 0
+#   range = (1..100).to_a 
+#   range.none? {|i| i % range == 0}
+#   else
+#     false
+#   end
+# end
