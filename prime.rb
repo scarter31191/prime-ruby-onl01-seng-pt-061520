e6e35c41db9b3d32efb80162cc3ad26633510365
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 20:51:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,18 @@
 def prime?(num)
   
-  
-  
-  i = 2 
-  if i > 0
-  range = (1..100).to_a 
-  range.none? {|i| i % range == 0}
-  else
-    false
+ if num <= 1
+   return false
+ else
+   range = (2.. num - 1)
+   range.none? {|i| num % i == 0}
   end
-end
+ end
+  
+#   i = 2 
+#   if i > 0
+#   range = (1..100).to_a 
+#   range.none? {|i| i % range == 0}
+#   else
+#     false
+#   end
+# end
